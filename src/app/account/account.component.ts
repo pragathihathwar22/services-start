67375ccba648fe2ff9089f54087290a432f4afc8
Hathwar, Praghathi (SHS TE DC IND DX QSI) <pragathi.hathwar@gmail.com>
2022-09-05 15:24:51 +0530

    Add project files.

---
+++ src/app/account/account.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LoggingService } from '../logging.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+
+  constructor(private accountService:AccountService){}
+
+
+  onSetTo(status: string) {
+    this.accountService.updateStatus(this.id,status);
+    this.accountService.statusUpdated.emit(status);
+  }
+}
